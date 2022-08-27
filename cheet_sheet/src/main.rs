b4721b9095ac8aa99e7a75678f6440ee3d3c34b8
josemasar <josemanuelsarrion@gmail.com>
2022-08-27 20:00:41 +0200

    first commit

---
+++ cheet_sheet/src/main.rs
@@ -0,0 +1,6 @@
+use var_types_func;
+
+fn main() {
+    println!("Hello, world!");
+    var_types_func::variables::learn_variables()
+}
---
+++ var_types_func/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod variables {
+    pub fn learn_variables(){
+    println!("It works");
+    }
+}
